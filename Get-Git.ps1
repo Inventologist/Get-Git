133e22c12bb667976e953f752ecdfe55ab53652f
Ben Therien <cs135cid@yahoo.com>
2020-07-28 16:00:46 -0400

    Create Get-Git.ps1

---
+++ Get-Git.ps1
@@ -0,0 +1,88 @@
+
+Param (
+[parameter()]$PSModulePath = ($profile | Split-Path) + "\Modules",
+[parameter(Mandatory)]$GHDLUri,
+[parameter(Mandatory)][string]$GHUser,
+[parameter(Mandatory)][string]$GHRepo,
+[parameter(Mandatory)][ValidateSet("Yes","No")]$ForceRefresh
+)
+    
+<#
+    .SYNOPSIS
+    Downloads and Expands a GitHub Repository so you can use components in your scripts and have them automatically load
+
+    .DESCRIPTION
+    Downloads a GitHub Repository ZIP file
+    Exapnds the Zip file
+    Renames it without the -master on the end
+
+    .PARAMETER PSModulePath
+    Specifies the location to store the modules.
+
+    .PARAMETER GHDLUri
+    The GitHub Download URI
+    Example: https://github.com/Inventologist/SuperLine/archive/master.zip
+
+    .INPUTS
+    None. You cannot pipe objects to this
+
+    .OUTPUTS
+    None.
+
+    .EXAMPLE
+    Example of what I use in my scripts to call this script and download other Repos
+    First Line Downloads and executes the scriptGETS the Repo and stores it in your Modules Directory
+    Second Line Dot Sources the script necessary to access the function (If it were a PSM1 file you would use: Import-Module $PathtoModule\NameOfPSMFile.psm1)
+        
+    Get-Git.ps1 -GHDLUri https://github.com/Inventologist/SuperLine/archive/master.zip -GHUser Inventologist -GHRepo SuperLine -ForceRefresh Yes
+    . $PathtoModule\Public\SuperLine.ps1
+
+
+    .LINK
+    Online version: http://www.fabrikam.com/extension.html
+#>
+
+$GHDLFile = Split-Path $GHDLUri -Leaf
+$Script:PathtoModule = "$PSModulePath\$GHRepo"
+
+Function GHDLRepo {
+    $GHDLRepo = ""
+    Write-Host "Downloading Repository Zip File for: $GHRepo"
+    Start-BitsTransfer -Source $GHDLUri -Destination $PSModulePath
+    If (Test-Path -Path $PSModulePath\$GHDLFile) {$Script:GHRepoDL = "Yes"} ELSE {Write-Error "Download Failure";$Script:GHRepoDL = "No"}
+}
+
+Function GHDLRefresh {
+    DO {
+        Write-Host "ForceRefresh is ON... Removing local repository for $GHRepo"
+        Remove-Item -Path $PSModulePath\$GHRepo -Force -Recurse
+    } UNTIL (!(Test-Path -Path $PSModulePath\$GHRepo))
+}
+    
+Function GHDLFinalize {
+    Write-Host "Expanding Repository Zip File"
+    Expand-Archive -Path $PSModulePath\$GHDLFile -DestinationPath $PSModulePath -Force
+    $Script:ExpandedDirName = (Get-Item -Path "$PSModulePath\$GHRepo-*").name
+    
+    Write-Host "Cleanup"
+    Rename-Item -Path $PSModulePath\$ExpandedDirName -NewName $PSModulePath\$GHRepo
+    Remove-Item -Path $PSModulePath\$GHDLFile
+}
+
+IF (!(Test-Path -Path $PSModulePath\$GHRepo)) {
+    GHDLRepo
+    IF ($GHRepoDL -eq "Yes") {GHDLFinalize}
+} ELSE {
+    #Repo Exists and ForceRefresh = NO
+    IF ($ForceRefresh -eq "No") {
+        Write-Warning "Module already Exists @ $PSModulePath\$GHRepo`nSkipping download"
+        Start-Sleep 1
+        return
+    }
+    #Repo does NOT exist and ForceRefresh = Yes
+    IF ($ForceRefresh -eq "Yes") {
+        GHDLRepo
+        IF ($GHRepoDL -eq "Yes") {GHDLRefresh}
+        IF ($GHRepoDL -eq "Yes") {GHDLFinalize}        
+    }
+}
