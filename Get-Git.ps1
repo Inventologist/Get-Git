fbbddd54ffe905419c711b684f49f77fcd91e4c7
Ben Therien <cs135cid@yahoo.com>
2020-07-28 21:30:21 -0400

    Update Get-Git.ps1

--- Get-Git.ps1
+++ Get-Git.ps1
@@ -1,48 +1,47 @@
+  
 <#
     This script is meant to be referenced inside of all your scripts that use other Git Repositories.
-
+    
     #############################################################################################################
     The idea is that WHEREVER your script goes, it is Self-Contained and Self-Updating as far as its Dependencies
     #############################################################################################################
-
+    
     It will download and keep current (ForceRefresh = ON) any GitHub Repo you specify.
-
+    
     ### After the process is done, you will need to MANUALLY Dot-Source a script or Import-Module on the proper item(s) in the Repo to activate it!!
     I am working on an automatic way to do it... more on that soon.
-
+    
     This script DOES NOT make the Functions inside the Repo available for immediate use... it makes them Available to Load into your environment.
-
     See Examples in the function comment block
 #>
-
 
 <#
     .SYNOPSIS
     Downloads and Expands a GitHub Repository so you can use components in your scripts and have them automatically load them
-
+    
     .DESCRIPTION
     Downloads a GitHub Repository ZIP file
     Exapnds the Zip file
     Renames it without the -master on the end
-
+    
     .PARAMETER PSModulePath
     Specifies the location to store the modules.
-
+    
     .PARAMETER GHDLUri
     The GitHub Download URI
     Example: https://github.com/Inventologist/SuperLine/archive/master.zip
-
+    
     .INPUTS
     None. You cannot pipe objects to this
-
+    
     .OUTPUTS
     None
-
+    
     .EXAMPLE
     Example of what I use in my scripts to call this script and download other Repos
     First Line Downloads and executes the script GETS the Repo and stores it in your Modules Directory
     Second Line Dot Sources the script necessary to access the function (If it were a PSM1 file you would use: Import-Module $PathtoModule\NameOfPSMFile.psm1)
-
+    
     Invoke-Expression ('$GHDLUri="https://github.com/Inventologist/SuperLine/archive/master.zip";$GHUser="Inventologist";$GHRepo="SuperLine"' + (new-object net.webclient).DownloadString('https://raw.githubusercontent.com/Inventologist/Get-Git/master/Get-Git.ps1'))
     . $PathtoModule\Public\SuperLine.ps1
 #>
@@ -51,7 +50,7 @@ $Global:ProgressPreference = 'SilentlyContinue'
 
 $PSModulePath = ($profile | Split-Path) + "\Modules"
 $GHDLFile = Split-Path $GHDLUri -Leaf
-$Script:PathtoModule = "$PSModulePath\$GHRepo"
+$Script:PathToModule = "$PSModulePath\$GHRepo"
 
 Function GHDLRepo {
     $GHDLRepo = ""
@@ -94,3 +93,11 @@ IF (!(Test-Path -Path $PSModulePath\$GHRepo)) {
         IF ($GHRepoDL -eq "Yes") {GHDLFinalize}
     }
 }
+
+IF (Test-Path $PathToModule\Get-Git.AutoLoad.txt) {
+    Write-Host "AutoLoad Command Found" -f DarkGray
+    $CommandToLoad = Get-Content $PathToModule\Get-Git.AutoLoad.txt
+    Invoke-Expression -Command $CommandToLoad
+}
+
+Write-Host ""
